e49c34988634af352f2d7efa3bc091c2f0ee79a4
Alan Spector <me@aj5.org>
2013-04-12 11:03:38 -0700

    meh

--- person.py
+++ person.py
@@ -6,8 +6,11 @@ class Person(object):
         self.firstName = None
         self.lastName = None
         self.avatar = None
+        self.currentSprint = None
+        self.pastSprints = []
         self.estimatedSprintHours = 0
         self.availableSprintHoursLeft = 0
+        self.notes = []
 
 
 
--- scrumboard.py
+++ scrumboard.py
@@ -1,28 +1,12 @@
 __author__ = 'alan'
 
-from UserDict import UserDict
 
-
-class ScrumBoard(UserDict):
+class ScrumBoard(object):
     def __init__(self):
-        UserDict.__init__(self)
-        self["cards"] = []
-        self["people"] = []
-        self["startDate"] = None
-        self["endDate"] = None
-        self["HoursPerDay"] = 4
-
-
-    def placeCard(self, card, location):
-        pass
-
-
-    def totalQAHours(self):
-        pass
-
-
-    def totalDevHours(self):
-        pass
-
+        self.cards = []
+        self.people = []
+        self.startDate = None
+        self.endDate = None
+        self.hoursPerDay = 4
 
 
