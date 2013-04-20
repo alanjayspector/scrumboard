adc0a493a5379af588f7eef65fd37a46e9887ef4
alanjayspector <me@aj5.org>
2013-04-20 03:49:06 -0500

    first upload!

--- person.py
+++ person.py
@@ -2,12 +2,14 @@ __author__ = 'alan'
 
 
 class Person(object):
+
     def __init__(self):
         self.firstName = None
         self.lastName = None
         self.avatar = None
         self.currentSprint = None
         self.pastCards = []
+        self.currentCards = []
         self.estimatedSprintHours = 0
         self.availableSprintHoursLeft = 0
         self.notes = []
