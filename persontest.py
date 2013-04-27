895551996a70d5fc96e9a6706324fdadaa8010be
alanjayspector <me@aj5.org>
2013-04-27 00:40:42 -0500

    starting on person unit tests

--- persontest.py
+++ persontest.py
@@ -6,6 +6,26 @@ from person import *
 import card
 from card import *
 
+class personHourChecks(unittest.TestCase):
+    pass
 
+
+
+
+
+
+
+
+class personVelocityChecks(unittest.TestCase):
+    pass
+
+
+
+
+
+
+
+class personColorCardChecks(unittest.TestCase):
+    pass
 
 
