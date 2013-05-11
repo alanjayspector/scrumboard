e3da767fc0d19972ca634864724c3c88fff05faa
Alan Spector <github@aj5.org>
2013-05-11 16:25:07 -0500

    fixing up cardtests with some of the changes i made in card. will be adding a couple of new ones next

--- cardtest.py
+++ cardtest.py
@@ -34,6 +34,11 @@ class cardPlacementChecks(unittest.TestCase):
         developer = Person({"firstName": "Alan", "lastName": "Spector"})
         self.card = Card({"description": "My Test Card", "qa": qa, "developer": developer})
 
+    def tearDown(self):
+        self.card.qa = None
+        self.card.developer = None
+        self.card = None
+
     def testDoneWithoutPoReview(self):
         self.card.needsCodeReview = False
         self.card.needsQA = False
@@ -69,6 +74,11 @@ class cardPlacementChecks(unittest.TestCase):
         self.assertEqual(True, testCard.hadCodeReview, "hadCodeReview was not set to True")
         testCard.placeOnBoard = "Development"
         self.assertEqual(False, testCard.hadCodeReview, "hadCodeReview was not set to False")
+
+    def testCardGoingToQAWithoutQAAssigned(self):
+        self.card.qa = None
+        with self.assertRaises(NoQAAssignedError):
+            self.card.placeOnBoard = "QA"
 
 
 if __name__ == "__main__":
