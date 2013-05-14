c9146a78c97d076d3bc326aaf5d6b5b80fe51eb6
Alan Spector <github@aj5.org>
2013-05-13 21:56:10 -0500

    starting work on scrumboard tests and sprint tests

--- scrumboardtest.py
+++ scrumboardtest.py
@@ -2,17 +2,35 @@ __author__ = 'alan'
 
 import unittest
 from scrumboard import Scrumboard
+from person import Person
+from card import Card
 import datetime
 
 
 class scrumboardTests(unittest.TestCase):
     def setUp(self):
         self.scrumboard = Scrumboard()
+        self.developer = Person({"firstName": "Alan", "lastName": "Spector", "estimatedSprintHours": 32})
+        self.qa = Person({"firstName": "Bob", "lastName": "Smith", "estimatedSprintHours": 16})
+        self.cards = [
+            Card({
+                "description": "As a user I want to fly.", "storyPoints": 5, "estimatedDevHours": 18,
+                "estimatedQAHours": 4
+            }),
+            Card({
+                "description": "As a user I want to dance.", "storyPoints": 20, "estimatedDevHours": 12,
+                "estimatedQAHours": 5
+            })
+        ]
+        self.scrumboard.assignPersonToScrumboard(self.developer)
+        self.scrumboard.assignPersonToScrumboard(self.qa)
+        for card in self.cards:
+            self.developer.assignCardToSelf(card)
+            self.qa.assignCardToSelf(card)
 
 
-    def testRedCards(self):
-        for card in self.scrumboard.reportRedCards(1):
-            print card
+    def testTotalStoryPoints(self):
+        pass
 
 
 suite = unittest.TestLoader().loadTestsFromTestCase(scrumboardTests)
